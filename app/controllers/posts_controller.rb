635b3dbbbd992fa342046b59759846d796759080
ono-max <nono19@students.desu.edu>
2020-01-31 12:23:04 +0900

    front implement and create controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body, :image, :email, :date, :select).merge(user_id: current_user.id)
+  end
+
 end
