550117ad7896bc6029cf3e264093c0576a28a296
ono-max <nono19@students.desu.edu>
2020-01-31 15:56:33 +0900

    update database

--- db/migrate/20200131064009_create_posts.rb
+++ db/migrate/20200131064009_create_posts.rb
@@ -2,9 +2,10 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.text :content
+      t.string :select
       t.string :image
       t.string :email
-      t.string :select
+      t.integer :date
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,10 @@ ActiveRecord::Schema.define(version: 2020_01_31_064009) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
+    t.string "select"
     t.string "image"
     t.string "email"
-    t.string "select"
+    t.integer "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
